1c1631cc1142a877ad8c137607d83e628a48a68d
bwettenstein <wettenstein@gmail.com>
2020-12-31 20:32:01 -0800

    Removed navbar for media queries

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ document.addEventListener('scroll', function () {
     document.querySelector('header').classList.remove('nav-scroll');
   }
 });
+
+// // hamburger menu event listener
+// document
+//   .querySelector('.hamburger-container')
+//   .addEventListener('click', function () {
+//     const hiddenElements = document.querySelectorAll('.hidden');
+//     const contentContainer = document.querySelectorAll('.content-container');
+
+//     if (hiddenElements.length === 0) {
+//       contentContainer.forEach((element) => {
+//         element.classList.add('hidden');
+//         element.style.pointerEvents = 'none';
+//       });
+
+//       // hamburgerNavContainer.style.display = 'unset';
+//     } else {
+//       contentContainer.forEach((element) => {
+//         element.classList.remove('hidden');
+//         element.style.pointerEvents = 'all';
+//       });
+
+//       // hamburgerNavContainer.style.display = 'none';
+//     }
+//   });
